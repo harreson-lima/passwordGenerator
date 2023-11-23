63a7844c6f1de74fd56ca497ce987abbe4f67d0b
Harreson Lima <contatoharresonlima@gmail.com>
2023-11-23 19:25:29 -0300

    finished project

--- js/index.js
+++ js/index.js
@@ -1,9 +1,51 @@
-let quant = document.getElementById("quantity");
-let quantDisplay = document.getElementById("quantity_display");
+const UPPER_CASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const LOWER_CASE = "abcdefghijklmnopqrstuvwxyz";
+const NUMBERS = "0123456789";
+const SYMBOLS = "!#$%&()*+-/?@_|";
 
-quantDisplay.innerHTML = quant.value;
-quant.addEventListener("input", print);
+const quantity = document.getElementById("quantity");
+const displayQuantity = document.getElementById("quantity_display");
 
-function print() {
-  quantDisplay.innerHTML = quant.value;
+displayQuantity.innerHTML = quantity.value;
+quantity.addEventListener("input", (e) => {
+  displayQuantity.innerHTML = e.target.value;
+});
+
+const passwordField = document.getElementById("password");
+const upperCaseEle = document.getElementById("uppercase");
+const lowerCaseEle = document.getElementById("lowercase");
+const numbersEle = document.getElementById("numbers");
+const symbolsEle = document.getElementById("symbols");
+const generateBtn = document.getElementById("generate-btn");
+generateBtn.addEventListener("click", generatePassword);
+const copyBtn = document.getElementById("copy");
+copyBtn.addEventListener("click", copyPassword);
+
+function generatePassword() {
+  let password = "";
+  let length = quantity.value;
+  let chars = "";
+
+  chars += upperCaseEle.checked ? UPPER_CASE : "";
+  chars += lowerCaseEle.checked ? LOWER_CASE : "";
+  chars += numbersEle.checked ? NUMBERS : "";
+  chars += symbolsEle.checked ? SYMBOLS : "";
+
+  if (chars !== "") {
+    for (let i = 0; i <= length; i++) {
+      let rand = Math.floor(Math.random() * chars.length);
+      password += chars.substring(rand, rand + 1);
+    }
+    passwordField.innerHTML = password;
+  } else {
+    alert("Please Select at least one type of character");
+  }
+}
+
+async function copyPassword() {
+  if (navigator.clipboard) {
+    await navigator.clipboard.writeText(passwordField.innerHTML);
+
+    alert("Password Copied!");
+  }
 }
